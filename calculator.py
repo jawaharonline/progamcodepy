43988651476857608a7780b8ff190deacccac5e4
Jawahar <90755327+jawaharonline@users.noreply.github.com>
2021-10-08 02:55:26 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,28 @@
+# Adding claculator
+print("Enter your first no.")
+a = int(input())
+print("Enter your second no.")
+b = int(input())
+print("Your sum is here: ", a+b)
+
+#  Multiplying calculator
+print("Enter the digit")
+c = int(input())
+print("Enter another digit ")
+d = int(input())
+print("Your product is here: ", c*d)
+
+# Divison clac
+print("Enter the digit")
+k = int(input())
+print("Enter another digit ")
+l = int(input())
+print("Your quotient is here: ", k/l)
+
+# Average calc
+a = int(input("enter 1st no:"))
+b = int(input("2nd no:"))
+a = int(a)
+b = int(b)
+avg = (a + b)/2
+print("the vaule is", avg)
